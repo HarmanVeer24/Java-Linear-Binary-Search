e98787f9d092c27609b15a053a82d60700f70d19
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-25 22:48:02 +0530

    [Harman] ADD:program to find the first and last occurance of element in array

---
+++ FirstLastOccurance.java
@@ -0,0 +1,55 @@
+public class FirstLastOccurance {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,6,6,7,8,9};
+        System.out.println(findFirstOccurence(arr,6));
+        System.out.println(findLastOccurence(arr,6));
+    }
+    public static int findLastOccurence(int[] arr,int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int last = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] == target){
+                last = mid;
+                left = mid + 1;
+            }
+            else if(arr[mid] > target){
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return last;
+    }
+
+    public static int findFirstOccurence(int[] arr,int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int first = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target){
+                first = mid;
+                right = mid - 1;
+            }
+
+            else if(arr[mid] < target){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return first;
+    }
+}
+//output
+// 4
+//5
